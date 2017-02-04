543146e949329d7f84f4bef67c63647cdadf40ce
blondegeek <blondegeek@gmail.com>
2017-02-04 11:00:14 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flatsurf',
+      version='0',
+      description='algorithms for flattening 3D surfaces to 2D',
+      url='http://github.com/blondegeek/flatsurf',
+      author='Tess Smidt',
+      author_email='tsmidt@berkeley.edu',
+      license='MIT',
+      packages=['flatsurf'],
+      zip_safe=False)
